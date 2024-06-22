634f5e456f7356a999bf8f5909173fd27e168b80
Robbie Sapunarich <robertsapunarich@gmail.com>
2024-06-22 12:55:08 -0500

    Get fetching from numbers API working. Need to parse response from numbers API to something more structured.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/robertsapunarich/number_facts_go
+
+go 1.22
+
+require github.com/sashabaranov/go-openai v1.26.0 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,62 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	http.HandleFunc("/number_facts", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(getNumberFact(r)))
+		w.Header().Set("Content-Type", "application/json")
+		response := getNumberFact(r)
+		err := json.NewEncoder(w).Encode(response)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	})
 	http.ListenAndServe(":8080", nil)
 }
 
-func getNumberFact(r *http.Request) string {
+func getNumberFact(r *http.Request) CustomResponse {
 	query := r.URL.Query().Get("query")
-	return "your query is " + query
+	parsed, err := strconv.Atoi(query)
+
+	if err != nil {
+		resp := CustomResponse{Message: "Invalid query"}
+		return resp
+	}
+
+	fact := getFactFromNumbersApi(parsed)
+	resp := CustomResponse{Message: fact}
+	return resp
+}
+
+func getFactFromNumbersApi(number int) string {
+	num64 := int64(number)
+	resp, err := http.Get("http://numbersapi.com/" + strconv.FormatInt(num64, 10) + "/trivia" + "?json")
+
+	if err != nil {
+		fmt.Println(err)
+		return "Error getting fact"
+	}
+
+	defer resp.Body.Close()
+
+	body := resp.Body
+	fact := make([]byte, 100)
+	_, err = body.Read(fact)
+
+	if err != nil {
+		return "Error reading fact"
+	}
+
+	return string(fact)
+}
+
+type CustomResponse struct {
+	Message string `json:"message"`
 }
